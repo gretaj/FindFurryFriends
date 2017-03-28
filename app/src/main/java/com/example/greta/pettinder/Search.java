e34d67e0ae51df206e69db70b275604fd0efb419
Greta Jarvi <Greta Jarvi>
2017-03-28 10:34:42 -0400

    Initial commit

---
+++ app/src/main/java/com/example/greta/pettinder/Search.java
@@ -0,0 +1,51 @@
+package com.example.greta.pettinder;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class Search extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_search:
+                    mTextMessage.setText(R.string.title_activity_search);
+                    return true;
+                case R.id.navigation_favs:
+                    mTextMessage.setText("faves");
+                    return true;
+                case R.id.navigation_profile:
+                    mTextMessage.setText("profile");
+                    return true;
+                case R.id.navigation_upload:
+                    mTextMessage.setText("upload");
+                    return true;
+                case R.id.navigation_notes:
+                    mTextMessage.setText("notifications");
+                    return true;
+            }
+            return false;
+        }
+
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
